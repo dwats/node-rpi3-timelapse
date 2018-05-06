21a951799554deae56ecab631efb5faf4867a04a
Eric Julius <ejulius86@gmail.com>
2018-05-06 14:18:52 -0500

    Addressed an issue where new images were overwritting previous images

--- timelapse/handleImage.js
+++ timelapse/handleImage.js
@@ -7,6 +7,7 @@ const path = require('path')
 const log = require('../utils/log')
 const pad = require('../utils/pad')
 
+const maxFrameCount = Number(process.env.MAX_FRAME_COUNT)
 const apiUrl = new URL(process.env.API_URL)
 const imageDir = path.join(__dirname, '../media/images')
 
@@ -21,30 +22,32 @@ async function handleImage () {
 }
 
 function getImage () {
-  const camera = new PiCamera({
-    mode: 'photo',
-    output: path.join(imageDir, 'frame_0391.png'),
-    encoding: 'png',
-    width: 1280,
-    height: 720,
-    nopreview: true
-  })
+  return fs.readdir(imageDir)
+    .then(files => {
+      const camera = new PiCamera({
+        mode: 'photo',
+        output: path.join(imageDir, `frame_${pad(files.length + 1, 4)}.png`),
+        encoding: 'png',
+        width: 1280,
+        height: 720,
+        nopreview: true
+      })
 
-  return camera.snap()
-    .then((res) => { log(`image captured`) })
+      return camera.snap()
+    })
+    .then(res => log(`image captured`))
     .catch(log)
 }
 
 const pruneOldestImage = () => {
   return fs.readdir(imageDir)
     .then(files => {
-      if (files.length > 390) {
+      if (files.length > maxFrameCount) {
         toDelete = path.join(imageDir, files.shift())
         log(`deleting ${toDelete}`)
         fs.unlinkSync(toDelete)
         return true
       }
-      return false
     })
     .catch(log)
 }
@@ -62,25 +65,25 @@ function renameImages () {
     .catch(log)
 }
 
-function sendLatestImage () {
-  return new Promise((resolve, reject) => {
-    const form = new FormData()
+// function sendLatestImage () {
+//   return new Promise((resolve, reject) => {
+//     const form = new FormData()
 
-    form.append('file', fs.createReadStream(path.join(imageDir, 'frame_0001.png')), {
-      filepath: output,
-      contentType: 'image/png'
-    })
-    const config = {
-      host: apiUrl.hostname,
-      path: '/image'
-    }
+//     form.append('file', fs.createReadStream(path.join(imageDir, 'frame_0001.png')), {
+//       filepath: output,
+//       contentType: 'image/png'
+//     })
+//     const config = {
+//       host: apiUrl.hostname,
+//       path: '/image'
+//     }
 
-    form.submit(config, (err, res) => {
-      if (err) return resolve(err, `\nerr @ ${new Date()}`)
-      if (!res) return reject(Error('No response.'))
-      return resolve(`submit ok @ ${new Date()}`)
-    })
-  })
-}
+//     form.submit(config, (err, res) => {
+//       if (err) return resolve(err, `\nerr @ ${new Date()}`)
+//       if (!res) return reject(Error('No response.'))
+//       return resolve(`submit ok @ ${new Date()}`)
+//     })
+//   })
+// }
 
 module.exports = handleImage
